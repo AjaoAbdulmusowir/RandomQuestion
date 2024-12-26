218cbdab4706ffd03855cff26b9b341a5d9c2955
AjaoAbdulmusowir <abdulmusowirajao@gmail.com>
2024-12-26 19:06:39 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, World!");
+string sentence = "Haffa Nah Long Time";
+string[] words = sentence.Split(' ');
+int count = 0;
+// Console.WriteLine(words.Length);
+foreach (string word in words)
+{
+    count++;
+}
+Console.WriteLine(count);
